006184477c99a3dc882d3228db65a19eeada4887
Ryan Sebastian <sebastiansryan@gmail.com>
2022-02-06 10:11:11 -0800

    implemented functionality for printContents, addToHead, addToTail, removeHead, removeTail, insertBefore, and insertAfter and created find as a helper method

---
+++ assignments/1-week/DoubleNode/src/DLLTester.java
@@ -0,0 +1,7 @@
+public class DLLTester {
+    public static void main(String[] args) {
+        DoublyLinkedList<String> list = new DoublyLinkedList<>();
+        list.addToHead("Hello");
+
+    }
+}
